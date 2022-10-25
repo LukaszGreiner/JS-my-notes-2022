bfb760f29caf5f09fd6935f758734861705956ca
Łukasz Greiner <devlukaszgreiner@gmail.com>
2022-10-25 22:47:30 +0200

    new page (timewatch) files added, basic skelet

---
+++ js/timewatch.js
@@ -0,0 +1,11 @@
+'use: strict'
+console.log('timewatch.js loaded');
+
+const currTimeSpan = document.getElementById('current-time-span');
+
+const timer = setInterval(displayTime, 500);
+function displayTime() {
+  const today = new Date();
+  const currTime = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
+  currTimeSpan.innerHTML = `🕐 ${currTime}`;
+}
