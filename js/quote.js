0304e601076e969d3d6aa4630c9dafc1bcaefb67
Łukasz Greiner <devlukaszgreiner@gmail.com>
2022-10-26 22:17:05 +0200

    buttons, next quote upon clicking button or pressing enter

--- js/quote.js
+++ js/quote.js
@@ -1,25 +1,61 @@
 //TODO Sharing quote on messenger
-//TODO Updating quote by clicking specific key or swaping right on mobile
+//TODO update quote by swaping right
 //TODO Creating you own quotes database and chosing source of quotes
 
-let quoteSpan = document.getElementById('quote-span');
-quoteSpan.innerHTML = '';
+// DOM elements
+const quoteSpan = document.getElementById('quote-span');
+const nextQuoteBtn = document.querySelector('#nextQuoteBtn');
+const saveQuoteBtn = document.querySelector('#saveQuoteBtn');
+const shareQuoteBtn = document.querySelector('#shareQuoteBtn');
+
+// Event listeners
+
+//shareQuoteBtn.addEventListener('click', shareQuote);
+//saveQuoteBtn.addEventListener('click', saveQuote);
+nextQuoteBtn.addEventListener('click', nextQuote);
+// next quote upon hitting Enter key
+window.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter')
+    nextQuote();
+})
+
+function nextQuote() {
+  nextQuoteBtn.disabled = true;
+  nextQuoteBtn.value = '...';
+  displayQuoteApi();
+  setTimeout(() => {
+    nextQuoteBtn.disabled = false;
+    nextQuoteBtn.value = 'Dalej';
+  }, 1000);
+}
 
 // Listy
-let quotesArray = [
+const quotesArray = [
   'Być, albo niebyć o to jest pytanie',
   'A miało być tak pięknie',
   'Jak to leciało?',
   'Ala ma kota',
 ];
-let userQuotesArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const userQuotesArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 // Zmienne
-let randomQ = randomInt(0, quotesArray.length - 1);
-let rapidApiQuote = '';
+const randomQuote = randomInt(0, quotesArray.length - 1);
+const rapidApiQuote = '';
 
 // rapidAPI || quotesArray || userQuotesArray
-let choice = 'rapidAPI';
+const choice = 'rapidAPI';
+
+// Head
+if (choice === 'rapidAPI') {
+  displayQuoteApi();
+} else if (choice === 'quotesArray') {
+  displayQuote(quotesArray, randomInt(quotesArray.length - 1));
+} else if (choice === 'userQuotesArray') {
+  displayQuote(userQuotesArray, randomInt(userQuotesArray.length - 1));
+} else {
+  console.log('Nieznana wartość zmiennej choice');
+}
+
 
 // Funkcje
 function randomInt(size) {
@@ -28,20 +64,10 @@ function randomInt(size) {
 
 function displayQuote(array, index) {
   const quote = array[index];
-  quoteSpan.innerHTML = quote;
+  quoteSpan.textContent = quote;
 }
 
-function addQuote() {
-  const quote = prompt('Dodaj swój cytat');
-  userQuotesArray.push(quote);
-
-  displayQuote(userQuotesArray);
-}
-
-//displayQuote(quotesArray, randomInt(quotesArray.length-1));
-
-if (choice === 'rapidAPI') {
-  // RapidAPI https://rapidapi.com/martin.svoboda/api/quotes15/
+function displayQuoteApi() {
   const options = {
     method: 'GET',
     headers: {
@@ -52,12 +78,20 @@ if (choice === 'rapidAPI') {
 
   fetch('https://quotes15.p.rapidapi.com/quotes/random/', options)
     .then(response => response.json())
-    .then(response => (quoteSpan.innerHTML = '“' + response.content + '”'))
+    .then(response => (quoteSpan.textContent = '“' + response.content + '”'))
     .catch(err => console.error(err));
-} else if (choice === 'quotesArray') {
-  displayQuote(quotesArray, randomInt(quotesArray.length - 1));
-} else if (choice === 'userQuotesArray') {
-  displayQuote(userQuotesArray, randomInt(userQuotesArray.length - 1));
-} else {
-  console.log('Nieznana wartość zmiennej choice');
 }
+
+
+function addQuote() {
+  const quote = prompt('Dodaj swój cytat');
+  userQuotesArray.push(quote);
+
+  displayQuote(userQuotesArray);
+}
+
+//displayQuote(quotesArray, randomInt(quotesArray.length-1));
+
+
+
+
