7484278018f001ebf3ce6a7ffe08e7a1cf567fb6
Łukasz Greiner <devlukaszgreiner@gmail.com>
2022-11-04 22:07:31 +0100

    camelcase code layout

--- js/sandbox.js
+++ js/sandbox.js
@@ -1,2 +1,17 @@
-'use:strict'
-console.log("sandbox.js loaded");
+'use:strict';
+
+document.article.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+// document.querySelector('textarea').value = '12adssdadas';
+
+// turns string into camelCase sentences
+String.prototype.camelCase = function () {
+  if (this.length === 0) return '';
+  const words = this.trim().split(' ');
+  const sentence = [];
+  for (let word of words) {
+    word = word.replace(word[0], word[0].toUpperCase());
+    sentence.push(word);
+  }
+  return sentence.join('');
+};
